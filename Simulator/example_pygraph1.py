7476bc2a4b4e7433d2ad921386e9fdd349f132ff
Przemyslaw Samsel <sp39427@zut.edu.pl>
2020-11-18 20:41:16 +0100

    Spotkanie 2

--- Simulator/example_pygraph1.py
+++ Simulator/example_pygraph1.py
@@ -11,9 +11,19 @@ app = QtGui.QApplication([])
 w = QtGui.QWidget()
 
 ## Create some widgets to be placed inside
-plot = pg.PlotWidget(title="Simple sine wave")
+cnv = pg.PlotWidget(title="Simple sine wave") # canvas?
 
-plot.plot([5], [5], QtGui.QBrush(QtGui.QColor(QtCore.qrand() % 256, QtCore.qrand() % 256, QtCore.qrand() % 256)))
+# graphWidget.setBackground('w') ?
+
+cnv.setXRange(0, 5)
+cnv.setYRange(0, 5)
+
+
+#plot.plot([5], [5], QtGui.QBrush(QtGui.QColor(QtCore.qrand() % 256, QtCore.qrand() % 256, QtCore.qrand() % 256)))
+pen1 = pg.mkPen(color=(255, 0, 0))
+#brush1 =
+pg.plot([2.5, 3, 4], [2.5, 3, 4], pen1)
+
 
 
 ## Create a grid layout to manage the widgets size and position
@@ -21,7 +31,8 @@ layout = QtGui.QGridLayout()
 w.setLayout(layout)
 
 ## Add widgets to the layout in their proper positions
-layout.addWidget(plot, 0, 0)   # button goes in upper-left
+layout.addWidget(cnv, 0, 0)   # button goes in upper-left
+# w.setCentralWidget(cnv)
 
 
 ## Display the widget as a new window
